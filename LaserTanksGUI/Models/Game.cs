3ff3edf49a1bd5968c9f7745d3a30a7d22ff0e1a
Ryan <ryan@Rywalts-MacBook-Pro-6.local>
2020-06-06 22:14:14 +0800

    added models for players and game. maybe redundant

---
+++ LaserTanksGUI/Models/Game.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace LaserTanksGUI.Models
+{
+    public class Game
+    {
+        public List<Player> Players { get; set; }
+        
+
+    }
+}
---
+++ LaserTanksGUI/Models/Player.cs
@@ -0,0 +1,14 @@
+namespace LaserTanksGUI.Models
+{
+    /// <summary>
+    /// Holds info for a a player
+    /// </summary>
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int MaxHealth { get; set; }
+        public int Score { get; set; }
+
+    }
+}
